0de6f798d62614cdd0a3efa95bc2e628e4d07ab7
CarlosAlvear <cdealvear@udesa.edu.ar>
2022-06-13 14:54:37 -0300

    creando posteos

---
+++ src/screens/NewPost.js
@@ -0,0 +1,67 @@
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
+import React, {Component} from 'react'
+import { auth, db } from '../firebase/config';
+
+class NewPost extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            message:'',
+        }
+    }
+
+    newPost(message){
+        db.collection('posts').add({
+            owner:auth.currentUser.email,
+            createdAt: Date.now(),
+            message:message,
+        })
+        .then(response => console.log(response))
+        .catch(error => console.log(error.message))
+    }
+
+    render(){
+
+        return (
+            <View>
+                    <Text>Enviar mensaje</Text>
+                    <TextInput 
+                    style={styles.textarea}
+                    onChangeText= {(text)=> this.setState({
+                        message: text
+                    })}
+                    value={this.state.message}
+                    />
+                    <TouchableOpacity
+                        style={styles.btn}
+                        onPress={() => {
+                            this.newPost(this.state.message)
+                            this.setState({
+                                message:'',
+                            })
+                            this.props.navigation.navigate('Home')
+                        }}
+                    >
+                        <Text>Enviar mensaje</Text>
+                    </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    textarea:{
+        borderWidth:1,
+        borderColor:'#c3c3c3',
+        height:'auto',
+        minHeight:60,
+        marginTop:10
+    },
+    btn:{
+        marginTop:16,
+        borderColor:'red',
+        borderWidth:1
+    }
+})
+
+export default NewPost
